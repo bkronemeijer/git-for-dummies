1bd7303721196d5cd4d2d3398d13b754d4cfe4a3
efth2007 <efth2007@hotmail.com>
2020-08-04 17:46:38 +0200

    Terminal component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LevelOne from './pages/LevelOne';
-import LevelTwo from './pages/LevelTwo';
-import LevelThree from './pages/LevelThree';
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LevelOne from "./pages/LevelOne";
+import LevelTwo from "./pages/LevelTwo";
+import LevelThree from "./pages/LevelThree";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route to="/" component={HomePage}/>
-        <Route to="/level-1" component={LevelOne}/>
-        <Route to="/level-2" component={LevelTwo}/>
-        <Route to="/level-3" component={LevelThree}/>
+        <Route path="/level-1" component={LevelOne} />
+        <Route path="/level-2" component={LevelTwo} />
+        <Route path="/level-3" component={LevelThree} />
+        <Route exact path="/" component={HomePage} />
       </Switch>
     </div>
   );
